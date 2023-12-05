d83c0c6e1676da35775e9f322ced2b82ffefa662
Johnson Chen <cyberiumdev@gmail.com>
2023-12-05 02:37:41 +0000

    feat: add logic to generate markdown tables

--- js/utils.js
+++ js/utils.js
@@ -23,9 +23,9 @@ const ROSTER_SCHEMA = [ // Column for...
 const COLUMNS = {
     votes_start: ROSTER_SCHEMA.indexOf('ballots')
 }
-for(let col of ROSTER_SCHEMA) { // Figure out the indexes for the spreadsheet columns
-    if(col === 'ballots') continue;
-    else if(ROSTER_SCHEMA.indexOf(col) < COLUMNS.votes_start) COLUMNS[col] = ROSTER_SCHEMA.indexOf(col);
+for (let col of ROSTER_SCHEMA) { // Figure out the indexes for the spreadsheet columns
+    if (col === 'ballots') continue;
+    else if (ROSTER_SCHEMA.indexOf(col) < COLUMNS.votes_start) COLUMNS[col] = ROSTER_SCHEMA.indexOf(col);
     else COLUMNS[col] = -1 * (ROSTER_SCHEMA.length - ROSTER_SCHEMA.indexOf(col));
 }
 
@@ -62,11 +62,48 @@ function capitalizeFirst(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
+function generateMarkdownTable(arrayObject) {
+    const arrays = Object.values(arrayObject);
+    const maxLength = Math.max(...arrays.map(arr => arr.length));
+
+    let table = '| ';
+
+    // Add headers
+    for (const key in arrayObject) {
+        if (arrayObject.hasOwnProperty(key)) {
+            table += `${key} | `;
+        }
+    }
+    table += '\n';
+
+    // Add header separator
+    for (const key in arrayObject) {
+        if (arrayObject.hasOwnProperty(key)) {
+            table += '| --- | ';
+        }
+    }
+    table += '\n';
+
+    // Add rows
+    for (let row = 0; row < maxLength; row++) {
+        for (const key in arrayObject) {
+            if (arrayObject.hasOwnProperty(key)) {
+                const value = arrayObject[key][row] !== undefined ? arrayObject[key][row] : '';
+                table += `| ${value} | `;
+            }
+        }
+        table += '\n';
+    }
+
+    return table;
+}
+
+
 const MOTION_TYPES = {
-    general:            {debate: 3, passage: 0.5},
-    constitutional:     {debate: 5, passage: 0.6},
-    treaty:             {debate: 5, passage: 0.5},
-    appointment:        {debate: 3, passage: 0.5},
-    appointment_crs:    {debate: 3, passage: 0.6},
-    recall:             {debate: 3, passage: 0.6}
+    general: { debate: 3, passage: 0.5 },
+    constitutional: { debate: 5, passage: 0.6 },
+    treaty: { debate: 5, passage: 0.5 },
+    appointment: { debate: 3, passage: 0.5 },
+    appointment_crs: { debate: 3, passage: 0.6 },
+    recall: { debate: 3, passage: 0.6 }
 }
--- js/vote-close.js
+++ js/vote-close.js
@@ -58,7 +58,7 @@ function generate() {
 
     createVisual(billCode, threshold, tally);
     id('bbcode-out').value = `[table=100][tr][tdc="2"][align=center][color=#109aed][size=xx-large][b]Final Result[/b][/size]\n[i]${billTitle}[/i][/color][/align][/tdc][/tr][tr][tdc="2"][align=center][img]ENTER LINK TO IMAGE AFTER UPLOAD HERE[/img][/align][/tdc][/tr][tr][td][align=center][color=#4572A7][size=large][b]Ayes: ${tally.aye}[/b][/size][/color][/align]\n[i]${(100.0 * tally.aye / (numMags - tally.absent)).toFixed(1)}% | ${(100.0 * tally.aye / (tally.aye + tally.nay)).toFixed(1)}% (discounting abstentions)[/i][/td][td][align=center][color=#AA4643][size=large][b]Nays: ${tally.nay}[/b][/size][/color][/align]\n[i]${(100.0 * tally.nay / (numMags - tally.absent)).toFixed(1)}% | ${(100.0 * tally.nay / (tally.aye + tally.nay)).toFixed(1)}% (discounting abstentions)[/i][/td][/tr][tr][tdc="2"]There were ${tally.abstain} Abstentions (${(100.0 * tally.abstain / (numMags - tally.absent)).toFixed(1)}%); ${tally.absent} magisters were absent. Thus, attendance for this vote was ${(100.0 * (numMags - tally.absent) / numMags).toFixed(1)}%.[/tdc][/tr][tr][tdc="2"][size=medium]In light of these results, the proposal [color=` + (tally.aye >= (tally.aye + tally.nay) * threshold ? '#017000][b]passes[/b][/color], having achieved' : '#C0392B][b]fails[/b][/color], having fallen short of') + ` the required majority (>${threshold * 100}%).[/size][/tdc][/tr][/table]`;
-    id('table-out').value = voteTables
+    id('table-out').value = generateMarkdownTable(voteTables);
     id('paste-votes').value = pasteVotesList.replace('\n', '');
 }
 
